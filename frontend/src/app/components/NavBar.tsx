8f86dd8a119e103c2a04792e146d9de3a6f45ae4
Imesh Bandara <imeshnbandara826@gmail.com>
2026-05-22 19:03:20 +0530

    created navbar

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchBar from "./SearchBar";
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
-  )
-}
+    <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
+      
+      {/* LEFT */}
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/logo.png"
+          alt="TrendLama"
+          width={36}
+          height={36}
+          className="w-6 h-6 md:w-9 md:h-9"
+        />
 
-export default Navbar
+        <p className="hidden md:block text-md font-medium tracking-wider">
+          LUXORA-CLOTHING.
+        </p>
+      </Link>
+
+      {/* RIGHT */}
+      <div className="">
+        <SearchBar/>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/app/components/SearchBar.tsx
@@ -0,0 +1,5 @@
+const SearchBar = ()=>{
+    return (
+        <div className="">SearchBar</div>
+    )
+}
